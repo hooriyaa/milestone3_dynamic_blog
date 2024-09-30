f58b8da8d36b840397e2296f289818d57315befd
Hooriya M. Fareed <parih3080@gmail.com>
2024-09-30 19:19:50 +0500

    push_changes

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import React from "react";
-import heroImage from "../../assets/images/hero1.jpg"; 
+import heroImage from "../../assets/images/hero1.jpg";
 import Link from "next/link";
 
 const Hero = () => {
@@ -10,13 +10,22 @@ const Hero = () => {
       <section className="text-gray-800 body-font text-lg font-serif font-bold bg-purple-100">
         <div className="container mx-auto flex px-5 py-3 md:flex-row flex-col justify-center items-center">
           {/* Left Content */}
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col  md:items-center md:text-left md:mb-0 items-center text-center">
-            <p className="pb-3 text-4xl text-purple-800 mt-14">Welcome to My Blog</p>
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 md:mb-12 flex flex-col  md:items-center md:text-left  items-center text-center">
+            <p className="pb-3 text-4xl text-purple-800 mt-14">
+              Welcome to My Blog
+            </p>
             <h1 className="title-font text-center mt-3 md:mb-10 text-lg font-medium text-black">
-              Discover insights, tutorials, and stories around web development, programming, and tech. Whether you are a developer or just starting your coding journey, you&#39;ll find tips, guides, and inspiration. Join the community and simplify complex topics while fueling your passion.
+              Discover insights, tutorials, and stories around web development,
+              programming, and tech. Whether you are a developer or just
+              starting your coding journey, you&#39;ll find tips, guides, and
+              inspiration. Join the community and simplify complex topics while
+              fueling your passion.
             </h1>
             <div className="flex justify-center items-center md:pt-1 pt-5">
-              <Link href={"/blog"} className="inline-flex bg-[#7A1CAC] text-white px-6 py-3 rounded-lg hover:bg-[#9737cb] transition-colors duration-300 shadow-md">
+              <Link
+                href={"/blog"}
+                className="inline-flex bg-[#7A1CAC] text-white px-6 py-3 rounded-lg hover:bg-[#9737cb] transition-colors duration-300 shadow-md"
+              >
                 Explore Now
               </Link>
             </div>
@@ -24,12 +33,11 @@ const Hero = () => {
           {/* Right Image */}
           <div className="lg:max-w-lg md:pr-20 lg:w-full md:w-1/2 w-5/6 rounded-2xl">
             <Image
-              className="object-contain md:h-[480px] h-[355px] rounded-3xl hover:scale-105 duration-500 hover:duration-500"
-              alt="Coding blog cover"
-              src={heroImage} // Use the imported image
+              className="object-contain md:h-[480px] h-[355px] rounded-3xl hover:scale-110 duration-500 hover:duration-500"
+              alt="hero blog image"
+              src={heroImage}
               width={500}
               height={340}
-              priority
             />
           </div>
         </div>
